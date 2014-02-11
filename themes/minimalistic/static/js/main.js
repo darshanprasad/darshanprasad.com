090bd8fd8d0bd327894810fe160c30f9a9a70e32
Darshan Prasad <dprasad@gmail.com>
2014-02-10 20:53:42 -0800

    Adding tab selection via js

--- themes/minimalistic/static/js/main.js
+++ themes/minimalistic/static/js/main.js
@@ -1,6 +1,12 @@
-<script>
 window.onload = function() {
 	var urlString = new String(window.location.href);
-	console.log(urlString);
+	var selectedTab = document.getElementsByClassName("selected");
+	
+	if (selectedTab.length <= 0) {
+		if (urlString.match("blog")) {
+			document.getElementById('blog-tab').className = "selected";
+		} else {
+			document.getElementById('home-tab').className = "selected";
+		}
+	}
 };
-</script>
